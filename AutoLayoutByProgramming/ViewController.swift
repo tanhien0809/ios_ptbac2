184d04c91cd88294935a4a8d5a9d7a29f58a54a2
Cntt11 <Admin11@Cntt11s-iMac.local>
2017-04-08 07:31:22 +0700

    Initial Commit

---
+++ AutoLayoutByProgramming/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutByProgramming
+//
+//  Created by Cntt11 on 4/8/17.
+//  Copyright © 2017 Cntt11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
